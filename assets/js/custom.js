0df3597355218a4b54c37c4d9f9c56f17398909b
Khadija Tasnim <khadijatasnim338@gmail.com>
2024-03-11 14:45:05 +0600

    add new code

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -70,7 +70,92 @@ var swiper = new Swiper(".home1-banner-slider", {
 
 });
 
+var swiper = new Swiper(".banner-slider2", {
+	slidesPerView: 1,
+	speed: 1500,
+	spaceBetween: 30,
+	loop: true,
+	autoplay: {
+		delay: 2500, // Autoplay duration in milliseconds
+		disableOnInteraction: false,
+	},
+	navigation: {
+		nextEl: ".next-2",
+		prevEl: ".prev-2",
+	},
 
+	breakpoints: {
+		280: {
+			slidesPerView: 1,
+		},
+		386: {
+			slidesPerView: 1,
+		},
+		576: {
+			slidesPerView: 2,
+			spaceBetween: 15,
+		},
+		768: {
+			slidesPerView: 2,
+			spaceBetween: 15,
+		},
+		992: {
+			slidesPerView: 3,
+			spaceBetween: 20,
+		},
+		1200: {
+			slidesPerView: 3,
+			spaceBetween: 15,
+		},
+		1400: {
+			slidesPerView: 3,
+		},
+	}
+
+});
+var swiper = new Swiper(".discover-slider", {
+	slidesPerView: 1,
+	speed: 1500,
+	spaceBetween: 30,
+	loop: true,
+	autoplay: {
+		delay: 2500, // Autoplay duration in milliseconds
+		disableOnInteraction: false,
+	},
+	navigation: {
+		nextEl: ".next-1",
+		prevEl: ".prev-1",
+	},
+
+	breakpoints: {
+		280: {
+			slidesPerView: 1,
+		},
+		386: {
+			slidesPerView: 1,
+		},
+		576: {
+			slidesPerView: 2,
+			spaceBetween: 15,
+		},
+		768: {
+			slidesPerView: 2,
+			spaceBetween: 15,
+		},
+		992: {
+			slidesPerView: 3,
+			spaceBetween: 20,
+		},
+		1200: {
+			slidesPerView: 3,
+			spaceBetween: 15,
+		},
+		1400: {
+			slidesPerView: 3,
+		},
+	}
+
+});
 
 new WOW().init();
 
